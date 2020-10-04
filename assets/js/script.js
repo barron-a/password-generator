af7128550e33e395470fffd74551c5f7f3f16c38
Adam Barron <adambarron@me.com>
2020-10-03 22:27:45 -0700

    establish variables for password options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
-// Greeting and verify user would like to generate a new password
-var newPassword = window.confirm("Welcome to Barron's Secure Password Generator! Would you like to generate a new password?")
+// Establish variables
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = "0123456789"
+var spCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+var pwdOptions = ""
 
-var pwdCriteria = function() {
+// Greeting and verify user would like to generate a new password
+var newPassword = window.confirm("Welcome to Barron's Secure Password Generator! Would you like to generate a new password?");
+
+var generatePassword = function() {
   if (newPassword) {
-    var pwdLength = window.prompt("Please select a password length between 8 and 128 characters.")
+    var pwdLength = parseInt(window.prompt("Please select a password length between 8 and 128 characters."));
+    console.log(pwdLength)
   } else {
     window.alert("Thanks for stopping by - come again soon!")
   }
-}
+};
 
 pwdCriteria()
 
