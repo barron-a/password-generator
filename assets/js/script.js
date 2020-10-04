c9b574c9896fb72883bb7474281d768802cc2446
Adam Barron <adambarron@me.com>
2020-10-03 19:54:41 -0700

    added greeting, prompt to generate new password and prompt for password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
-// Assignment code here
+// Greeting and verify user would like to generate a new password
+var newPassword = window.confirm("Welcome to Barron's Secure Password Generator! Would you like to generate a new password?")
 
+var pwdCriteria = function() {
+  if (newPassword) {
+    var pwdLength = window.prompt("Please select a password length between 8 and 128 characters.")
+  } else {
+    window.alert("Thanks for stopping by - come again soon!")
+  }
+}
+
+pwdCriteria()
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
